eba1ea0d288dcfa875b0d01070bb303ca14c0ec1
Muhammad Quwais Safutra <45951646+BRAKOMANDA@users.noreply.github.com>
2019-01-05 16:03:35 +0700

    Create Helloworld.cpp

---
+++ Acunetix/Frameworks GenZUI/Frameworks 1.0/Helloworld.cpp
@@ -0,0 +1,38 @@
+
+#include "Frameworks"
+using __linux__ // Nama os yang anda pakai
+void BuatForm(__init__){
+   struct GZ ($__GETCHO) {
+        FrameworksGenZui _init_
+   }
+   std::hold    // memulai eksekusi dari GZ::Borders
+   GZ::Borders = {
+      _colors_ = '#red' // warna border
+      _visual_ = __3D // 3D
+      _titled_ = 'frameworks 1.0 By Muhammad Quwais Safutra' // Title
+  }
+  GZ::BY_THE_POSITION = {
+     _230_ // left,right
+     _400_ // top, down
+  }
+  GZ::ADD_MULTIMN = {
+     _justtext_ = "hello world!"  // menampilkan pesan hello world
+     /* Perlu kamu ketahui. Agar semua nya bisa berlanjut
+       * kamu perlu membuat spasi minimal 3 kali
+       */
+          _fg_ = '#red' // warna font
+          _fonttype_ = "bold" // bold font
+          _fontVariants_ = $__TAHOMA   // font gaya tahoma
+} => showinthedeep $_AS->(*hold)
+/* Di atas. Hanyalah untuk melakukan setup lanjutan saja */
+
+#define PLAY0x1  /* run app */
+    setup->BuatForm(true)
+    /*
+     * true : menunjukan border '_' '[]' 'X'
+     * false : tidak menunjukan semua border
+     * disable_stripts : tidak menujukan '_'
+     * disable_engkong : tidak menunjukan '[]'
+     * disable_closingdown : tidak menunjukan 'X'
+     * disabled_<nama border> : menunjukan. Tetapi fungsinya akan di matikan
+     */
