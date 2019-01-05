4b14e66c6e42b2673cf2a18dd573a6511d8db3da
Muhammad Quwais Safutra <45951646+BRAKOMANDA@users.noreply.github.com>
2019-01-05 15:56:39 +0700

    Create membuat_form.cpp

---
+++ Acunetix/Frameworks GenZUI/Frameworks 1.0/membuat_form.cpp
@@ -0,0 +1,27 @@
+#include "Frameworks"
+using __linux__ // Nama os yang anda pakai
+void BuatForm(__init__){
+   struct GZ ($__GETCHO) {
+        FrameworksGenZui _init_
+   }
+   GZ::Borders = {
+      _colors_ = '#red' // warna border
+      _visual_ = __3D // 3D
+      _titled_ = 'frameworks 1.0 By Muhammad Quwais Safutra' // Title
+  }
+  GZ::BY_THE_POSITION = {
+     _230_ // left,right
+     _400_ // top, down
+  }
+}
+
+#define PLAY0x1  /* run app */
+    setup->BuatForm(true)
+    /*
+     * true : menunjukan border '_' '[]' 'X'
+     * false : tidak menunjukan semua border
+     * disable_stripts : tidak menujukan '_'
+     * disable_engkong : tidak menunjukan '[]'
+     * disable_closingdown : tidak menunjukan 'X'
+     * disabled_<nama border> : menunjukan. Tetapi fungsinya akan di matikan
+     */
